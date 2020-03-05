05d27f1c9a754812a15acdab3d1aba0103c5efb0
Michael Patellis <mpatellis14@gmail.com>
2020-03-05 18:20:51 -0500

    Added process file function and removed previous count line function

--- assignment-3/problem-2/word-count-thread.cpp
+++ assignment-3/problem-2/word-count-thread.cpp
@@ -5,21 +5,48 @@ int get_num_cores()
     return sysconf(_SC_NPROCESSORS_ONLN);
 }
 
-void process_file(FILE* file)
-{
-
-}
-
-int count_lines(FILE* file)
+int process_file(std::string fn)
 {
     int num_lines = 0;
-    int nl;
+    std::ifstream read_file;
+    std::ofstream out_file;
 
-    while (EOF != (nl=getc(file)))
+    read_file.open(fn);
+    if (!read_file.is_open())
     {
-        if ('\n' == nl)
-            num_lines++;
+        std::cout << "Opening read file failed\n";
+        return -1;
     }
+
+    out_file.open(OUT_FILE, std::ios::in | std::ios::trunc);
+    if (!out_file.is_open())
+    {
+        std::cout << "Opening output file failed\n";
+        read_file.close();
+        return -1;
+    }
+
+    std::string line;
+    while (getline(read_file,line))
+    {
+        std::string process_line;
+        int str_len = line.size();
+
+        for (int i = 0; i < str_len; i++)
+        {
+            if (isalpha(line[i]))
+                process_line = process_line + (char)tolower(line[i]);
+            else if (line[i] == ' ')
+                process_line = process_line + line[i];
+        }
+
+        process_line.append("\n");
+        out_file << process_line;
+        num_lines++;
+    }
+
+    read_file.close();
+    out_file.close();
     return num_lines;
 }
 
@@ -32,16 +59,11 @@ int main(int argc, char* argv[])
     }
 
     long num_cores = get_num_cores();
-    std::cout << "Number of cores: " << num_cores << std::endl;
 
-    FILE* file = fopen(argv[1], "r");
-    process_file(file);
-    printf("%d\n", count_lines(file));
+    std::string file_name = argv[1];
+    int num_lines = process_file(file_name);
 
 
-    // read file
-    // process file
-    // find num lines
     // set up threads
     // map for word occurences
     return 0;
--- assignment-3/problem-2/word-count-thread.hpp
+++ assignment-3/problem-2/word-count-thread.hpp
@@ -3,5 +3,10 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <string>
+#include <fstream>
+
+#define OUT_FILE "output.txt"
 
 int get_num_cores();
+int process_file(std::string fn);
